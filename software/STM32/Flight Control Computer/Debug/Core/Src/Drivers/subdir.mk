23a5cc4d34bacd1e7c8a22a35e55f200e5650a3f
Nikolai Philipenko <nikolai.philipenko@gmail.com>
2024-10-26 20:10:05 -0600

    starting bmi driver...

---
+++ software/STM32/Flight Control Computer/Debug/Core/Src/Drivers/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Drivers/bmi088.c 
+
+OBJS += \
+./Core/Src/Drivers/bmi088.o 
+
+C_DEPS += \
+./Core/Src/Drivers/bmi088.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/Drivers/%.o Core/Src/Drivers/%.su Core/Src/Drivers/%.cyclo: ../Core/Src/Drivers/%.c Core/Src/Drivers/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32G474xx -c -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src-2f-Drivers
+
+clean-Core-2f-Src-2f-Drivers:
+	-$(RM) ./Core/Src/Drivers/bmi088.cyclo ./Core/Src/Drivers/bmi088.d ./Core/Src/Drivers/bmi088.o ./Core/Src/Drivers/bmi088.su
+
+.PHONY: clean-Core-2f-Src-2f-Drivers
+
